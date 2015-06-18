747c404c006139a9079b96c485e379d4c55a7cb4
iPet <patrick-salij@hotmail.com>
2015-06-18 17:05:46 +0200

    fix lost method status worker

--- app/workers/container_status_worker.rb
+++ app/workers/container_status_worker.rb
@@ -1,26 +1,34 @@
 class ContainerStatusWorker
-  require 'net/http'
-  require 'json'
-  require 'aes'
-  require 'action_view'
+	require 'net/http'
+	require 'json'
+	require 'aes'
+	require 'action_view'
 
-  def self.perform
-    update_all_containers
-  end
+	def self.perform
+		update_all_containers
+	end
 
-  ##
-  #Class methods 
-  def self.update_all_containers
+	##
+	#Class methods 
+	def self.update_all_containers
+		require 'docker'	
+		servers = Server.all
+		servers.each do |s| 
+			if s.daemon_status
+				Docker.url = "tcp://"+ s.host + ":5555"
+				all_containers = Docker::Container.all(:all => false)
+				all_containers.each do |c|
+					Container.parse_container(c.json, s)
+				end
+			end
+		end
+	end
+  def self.update_single_container(id, s)
     require 'docker'  
-    servers = Server.all
-    servers.each do |s| 
-      if s.daemon_status
-        Docker.url = "tcp://"+ s.host + ":5555"
-        all_containers = Docker::Container.all(:all => false)
-        all_containers.each do |c|
-          Container.parse_container(c.json, s)
-        end
-      end
+    if s.daemon_status
+      Docker.url = "tcp://"+ s.host + ":5555"
+      result = Docker::Container.get(id)
+      Container.parse_container(result.json, s)
     end
   end
 end
