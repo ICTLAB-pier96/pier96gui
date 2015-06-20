49d0abcc93b17b7e8497947168c8750a6f4c9c3f
iPet <patrick-salij@hotmail.com>
2015-06-20 15:01:31 +0200

    protect against attacks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
+  before_action :protect
+
+  protected
+    def protect
+      @ips = ['127.0.0.1', '62.195.198.94', '83.128.202.205']
+      if not @ips.include? request.remote_ip
+         if user = authenticate_with_http_basic { |u, p| u=='pier96' and p=='Pier96hro' }
+              @current_user = user
+         else
+              request_http_basic_authentication
+         end
+      end
+    end
 end
